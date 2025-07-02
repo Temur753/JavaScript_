bcdb3556c3a56f6931f56cca011bd4b3051e9a71
Temur753 <akhtamovtemur378@gmail.com>
2025-07-02 15:57:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting");
+let id = prompt("ID raqamingizni kiriting");
+let parol = prompt("Parolingizni kiriting");
+
+let obj = {}
+
+obj.ism = ism;
+obj.id = id;
+obj.parol = parol;
+
+console.log(obj);
